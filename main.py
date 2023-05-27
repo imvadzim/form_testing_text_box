ac6522860856ccd2cf8df38a87230cd04335dea5
Vadzim Novikau <5254220@gmail.com>
2023-05-27 21:18:23 +0530

    added a task

---
+++ main.py
@@ -0,0 +1,30 @@
+# Ссылка на страницу Text Box: https://demoqa.com/text-box
+#
+# Форма: Text Box
+#
+# Задание:
+# Написать фреймворк для тестирования этой формы с использованием шаблон проектирования PageObject.
+#
+# Короткий план:
+# Заполнить все поля рандомными данными (можно использовать дополнительные библиотеки).
+# Нажать кнопку Submit
+# Сравнить введенные данные с полученными данными из поля в котором будут все заполненные данные.
+#
+# Фреймворк закинуть в репозиторий и дать ссылку.
+
+
+# Import all necessary libraries
+
+
+# Create BasePage class
+
+
+# Create TextBoxPage class
+# Create separate methods for filling the form
+
+
+# Create submit test
+
+
+# Initialization WebDriver with the Chrome browser
+# attempt to fill the form and submit
